f5a8b9d74152292254eeb1308ef123bcddb6dcc8
audiophrases <emonfor3@xtec.cat>
2025-12-16 16:55:30 +0100

    Delete src directory

--- src/utils/answers.ts
+++
@@ -1,7 +0,0 @@
-const normalize = (text?: string) => text?.replace(/[’']/g, "'").trim().toLowerCase();
-
-export const isCorrectAnswer = (input: string, expected?: string[]) => {
-  if (!expected || !expected.length) return false;
-  const normalizedInput = normalize(input);
-  return expected.some((ans) => normalize(ans) === normalizedInput);
-};
